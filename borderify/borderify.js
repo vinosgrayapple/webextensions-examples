3b8402e74bbc8ac93a59abc9030ee780f2d8db9c
Serhii Komarychev <komarichev@gmail.com>
2020-01-18 01:34:00 +0200

    input change

--- borderify/borderify.js
+++ borderify/borderify.js
@@ -4,7 +4,7 @@ Just draw a border round the document.body.
 // document.body.style.border = "10px solid red";
 // parent.frames[1].document.body.style.border = '10px solid red'
 // let content = parent.frames[1].document.body
-  // content.style.border = '10px solid red'
+// content.style.border = '10px solid red'
 let style = document.createElement('style')
 document.body.appendChild(style)
 
@@ -15,8 +15,15 @@ browser.storage.onChanged.addListener((changes, area) => {
 })
 
 function update(value) {
-  // style.innerText = `html {filter: sepia(${value}%) !important}`
-  let content = parent.frames[1].document.body
-  content.style.border = `${~~(value/10)}px solid red`
+  const content = [
+    ...parent.frames[1].document.querySelectorAll('#DLLTable tr')
+  ]
+  const re = new RegExp(value, 'i')
+  content.forEach(el => {
+    if (!re.test(el.children[5].innerText.trim())) {
+      el.remove()
+    }
+  })
+  // content.style.border = `${~~(value / 10)}px solid red`
 }
 browser.storage.local.get('value').then(res => update(res.value))
--- borderify/popup.js
+++ borderify/popup.js
@@ -1,19 +1,61 @@
 let input = document.querySelector('input')
 
-input.addEventListener('change', e => setValue(e.target.value))
+input.addEventListener('input', e => {
+  const valueFrominput = e.target.value
+  if (valueFrominput.length >= 3) {
+    setValue(valueFrominput)
+    console.log(valueFrominput)
+  }
+})
 
 async function setValue(value) {
-  await browser.storage.local.set({value})
+  await browser.storage.local.set({ value })
 }
 
 async function init() {
   let { value } = browser.storage.local.get('value')
   if (!value) {
-    value = 0
+    value = ''
   }
-  input.value = value;
-  setValue(0)
+  input.value = value
+  setValue('')
+
+  // storage contains two items,
+  // "kitten" and "monster"
+  function setItem() {
+    console.log('OK')
+  }
+
+  function gotKitten(item) {
+    console.log(`${item.kitten.name} has ${item.kitten.eyeCount} eyes`)
+  }
+
+  function gotMonster(item) {
+    console.log(`${item.monster.name} has ${item.monster.eyeCount} eyes`)
+  }
+
+  function onError(error) {
+    console.log(error)
+  }
+
+  // define 2 objects
+  var monster = {
+    name: 'Kraken',
+    tentacles: true,
+    eyeCount: 10
+  }
+
+  var kitten = {
+    name: 'Moggy',
+    tentacles: false,
+    eyeCount: 2
+  }
+
+  // store the objects
+  browser.storage.local.set({ kitten, monster }).then(setItem, onError)
+
+  browser.storage.local.get('kitten').then(gotKitten, onError)
+  browser.storage.local.get('monster').then(gotMonster, onError)
 }
 
-init()
-.catch(err => console.error(err))
+init().catch(err => console.error(err))
